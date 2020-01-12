12cfd3e1db3d2cc9371f0e7c30f9625e2df593b5
Kamil <kamilworona@gmail.com>
2020-01-12 19:41:00 +0100

    check pdf certificate

--- src/test/java/pdfvalidator/TextValidation.java
+++ src/test/java/pdfvalidator/TextValidation.java
@@ -1,18 +1,31 @@
 package pdfvalidator;
 
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.security.CertificateInfo;
+import com.itextpdf.text.pdf.security.PdfPKCS7;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.Principal;
+import java.security.Security;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 import java.util.stream.Stream;
+
 
 public class TextValidation {
 
-    @Test
+  //  @Test
     public void getPdfAsText() {
         System.out.println();
 
@@ -26,7 +39,7 @@ public class TextValidation {
         }
     }
 
-    @Test
+   // @Test
     public void getSecondPageAsText() throws IOException {
         System.out.println();
 
@@ -45,7 +58,7 @@ public class TextValidation {
         }
     }
 
-    @Test
+  //  @Test
     public void getSecondPageAsTextAndCheckName() throws IOException {
 
         String filename = getClass().getClassLoader().getResource("pdfToValidate.pdf").getPath();
@@ -68,7 +81,49 @@ public class TextValidation {
         }
     }
 
+    @Test
+    public void checkPdfCertificate() throws IOException, GeneralSecurityException {
+        InputStream is = TextValidation.class.getClassLoader()
+                .getResourceAsStream("jadlospisSigned.pdf");
+        PdfReader reader = new PdfReader(is);
+        boolean ok = verifySignature(reader);
+       // LOGGER.info("'{}' is {}signed", "jadlospisSigned.pdf", ok ? "" : "NOT ");
+    }
 
+
+    public static final boolean verifySignature(PdfReader pdfReader)
+            throws GeneralSecurityException, IOException {
+        boolean valid = false;
+        Security.addProvider(new BouncyCastleProvider());
+        AcroFields acroFields = pdfReader.getAcroFields();
+        List<String> signatureNames = acroFields.getSignatureNames();
+        acroFields.getFields().values().stream().forEach(System.out::println);
+        System.out.println(signatureNames.size());
+        System.out.println("signature: "+signatureNames.get(0));
+        System.out.println(acroFields.signatureCoversWholeDocument(signatureNames.get(0)));
+        PdfPKCS7 pk = acroFields.verifySignature(signatureNames.get(0));
+        System.out.println("Subject: " + CertificateInfo.getSubjectFields(pk.getSigningCertificate()));
+        System.out.println("Issuer: " + CertificateInfo.getIssuerFields(pk.getSigningCertificate()));
+        System.out.println("Document verifies: " + pk.verify());
+//        if (!signatureNames.isEmpty()) {
+//            for (String name : signatureNames) {
+//                if (acroFields.signatureCoversWholeDocument(name)) {
+//                    PdfPKCS7 pkcs7 = acroFields.verifySignature(name);
+//                    valid = pkcs7.verify();
+//                    String reason = pkcs7.getReason();
+//                    Calendar signedAt = pkcs7.getSignDate();
+//                    X509Certificate signingCertificate = pkcs7.getSigningCertificate();
+//                    Principal issuerDN = signingCertificate.getIssuerDN();
+//                    Principal subjectDN = signingCertificate.getSubjectDN();
+//                    System.out.println(issuerDN.toString());
+////                    LOGGER.info("valid = {}, date = {}, reason = '{}', issuer = '{}', subject = '{}'",
+////                            valid, signedAt.getTime(), reason, issuerDN, subjectDN);
+//                    break;
+//                }
+//            }
+//        }
+        return valid;
+    }
     static String getText(File pdfFile) throws IOException {
         PDDocument doc = PDDocument.load(pdfFile);
         return new PDFTextStripper().getText(doc);
